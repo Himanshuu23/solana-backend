a1396a20bb6e90609fccd65fe5bb8f1d08d0a6fb
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-30 22:48:34 +0530

    exported the PORTS

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() {
         .route("/send/sol", post(send_sol))
         .route("/send/token", post(send_token));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("✅ Server running at http://{}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
